34ed4faa91d87dc5d60cb54d5144487bea74073a
Arya Auti <aryaautiwork@gmail.com>
2026-09-18 20:51:59 +0530

    Phase 1: build Satella components/home/RoomGrid.tsx

--- components/home/RoomGrid.tsx
+++ components/home/RoomGrid.tsx
@@ -5,23 +5,19 @@ import MusicRoom from "../rooms/MusicRoom"
 import ChatRoom from "../rooms/ChatRoom"
 import NotesRoom from "../rooms/NotesRoom"
 
+const rooms = [
+  ["watch", WatchRoom],
+  ["music", MusicRoom],
+  ["chat", ChatRoom],
+  ["notes", NotesRoom],
+] as const
+
 export default function RoomGrid() {
   return (
-    <div
-      className="
-      mt-8
-      grid
-      grid-cols-2
-      gap-4
-      "
-    >
-      <WatchRoom />
-
-      <MusicRoom />
-
-      <ChatRoom />
-
-      <NotesRoom />
+    <div className="mt-5 grid grid-cols-2 gap-3 sm:gap-4">
+      {rooms.map(([key, Room]) => (
+        <Room key={key} />
+      ))}
     </div>
   )
 }
